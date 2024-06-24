755111734280afd01ca28d4cc1b2c60904d3e6dd
Dominik Kaiser <dkaiser@1-C-7.42heilbronn.de>
2024-06-24 18:19:44 +0200

    Fix norme issues in minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 17:14:49 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/06/24 18:05:08 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/06/24 18:17:02 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 #  define DEBUG 0
 # endif
 
-#include "libft.h"
+# include "libft.h"
 
-void dbg(char *str);
+void	dbg(char *str);
 
 #endif
