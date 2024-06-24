b794f05bd955fd9cd2c6a640cd7694e1bf9b8161
Dominik Kaiser <dkaiser@1-C-7.42heilbronn.de>
2024-06-24 17:11:41 +0200

    Add Ctrl-D handling

--- src/repl.c
+++ src/repl.c
@@ -6,12 +6,11 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 16:07:04 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/06/24 16:13:37 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/06/24 16:50:39 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <readline/readline.h>
 
 void	repl(const char *prompt)
 {
@@ -20,5 +19,7 @@ void	repl(const char *prompt)
 	while (1)
 	{
 		input = readline(prompt);
+		if (input == NULL)
+			return ;
 	}
 }
