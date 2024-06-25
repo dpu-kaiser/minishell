28e021a967f5ea1290fdd22d25f6f5a7216cfa92
Dominik Kaiser <dkaiser@1-C-7.42heilbronn.de>
2024-06-25 12:37:10 +0200

    Resolve weird readline error
    Somehow readline doesn't work if it is included before stdio.h.
    This doesn't really make any sense to me as readline.h already includes
    stdio.h.

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 17:14:49 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/06/25 12:21:26 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/06/25 12:31:44 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 # define MINISHELL_H
 # include "debug_tools.h"
 # include "libft.h"
+# include <stdio.h>
 # include <readline/readline.h>
 # include <signal.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <termios.h>
 # include <unistd.h>
