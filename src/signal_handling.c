c88d28cea7b8b1ed16c842f8f5bde473a8b7e1d1
Dominik Kaiser <dkaiser@1-C-7.42heilbronn.de>
2024-06-25 13:36:20 +0200

    Add Ctrl-\ handling

--- src/signal_handling.c
+++ src/signal_handling.c
@@ -6,17 +6,20 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 15:08:43 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/06/25 12:26:57 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/06/25 13:33:26 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static void	handle_sigint(int status);
+static void	handle_sigquit(int status);
 
 int	init_signal_handling(void)
 {
 	if (signal(SIGINT, &handle_sigint) == SIG_ERR)
+		return (1);
+	if (signal(SIGQUIT, &handle_sigquit) == SIG_ERR)
 		return (1);
 	return (0);
 }
@@ -26,5 +29,10 @@ static void	handle_sigint(__attribute__((unused)) int status)
 	write(1, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
+	rl_redisplay();
+}
+
+static void	handle_sigquit(__attribute__((unused)) int status)
+{
 	rl_redisplay();
 }
