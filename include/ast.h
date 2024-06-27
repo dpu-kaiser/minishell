70b3652a922c08e95a3e886c3b8068bf34c9a260
Dominik Kaiser <dkaiser@student.42heilbronn.de>
2024-06-27 12:18:26 +0000

    Add data structures and helper functions for ast

---
+++ include/ast.h
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 11:48:27 by dkaiser           #+#    #+#             */
+/*   Updated: 2024/06/27 12:16:18 by dkaiser          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct s_sequence {
+    struct s_node **nodes;
+} t_sequence;
+
+enum e_node_type {
+ASSIGN,
+PIPE,
+CMD,
+STRING
+};
+
+typedef struct s_assign {
+    char *var;
+    char *value;
+} t_assign;
+
+typedef struct s_pipe {
+    struct s_node *left;
+    struct s_node *right;
+} t_pipe;
+
+enum e_redirection_type {
+INPUT_FILE,
+INPUT_LIMITER,
+OUTPUT_OVERRIDE,
+OUTPUT_APPEND
+};
+
+typedef struct s_redirection {
+    int type;
+    char *specifier;
+} t_redirection;
+
+typedef struct s_cmd {
+    char **args;
+    struct s_redirection redirs[2];
+} t_cmd;
+
+union u_node_content {
+    struct s_assign assign;
+    struct s_pipe pipe;
+    struct s_cmd cmd;
+    char *string;
+};
+
+typedef struct s_node {
+    int type;
+    union u_node_content content;
+} t_node;
+
+
+t_node *new_node(int type);
+t_node *new_assign_node(char *var, char *value);
+t_node *new_pipe_node(t_node *left, t_node *right);
+t_node *new_cmd_node(char **args, t_redirection redirs[2]);
+t_node *new_string_node(char *string);
---
+++ src/new_node.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   new_node.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 11:21:03 by dkaiser           #+#    #+#             */
+/*   Updated: 2024/06/27 12:18:07 by dkaiser          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ast.h"
+
+t_node *new_node(int type)
+{
+    t_node *node;
+
+    node = malloc(sizeof(t_node));
+    if (node == NULL)
+        return (NULL);
+    node->type = type;
+    return (node);
+}
+
+t_node *new_assign_node(char *var, char *value)
+{
+    t_node *node;
+
+    node = new_node(ASSIGN);
+    if (node == NULL)
+        return (NULL);
+    node->content.assign.var = var;
+    node->content.assign.value = value;
+    return (node);
+}
+
+t_node *new_pipe_node(t_node *left, t_node *right)
+{
+    t_node *node;
+
+    node = new_node(PIPE);
+    if (node == NULL)
+        return (NULL);
+    node->content.pipe.left = left;
+    node->content.pipe.right = right;
+    return (node);
+}
+
+t_node *new_cmd_node(char **args, t_redirection redirs[2])
+{
+    t_node *node;
+
+    node = new_node(CMD);
+    if (node == NULL)
+        return (NULL);
+    node->content.cmd.args = args;
+    node->content.cmd.redirs = redirs;
+    return (node);
+}
+
+t_node *new_string_node(char *string)
+{
+    t_node *node;
+
+    node = new_node(STRING);
+    if (node == NULL)
+        return (NULL)
+    node->content.string = string;
+    return (node);
+}
