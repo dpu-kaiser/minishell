8469f6ee45ac4125d649a9b4c2a6e4bc4711b046
Dominik Kaiser <dkaiser@1-E-17.42heilbronn.de>
2024-07-22 17:16:21 +0200

    Make ready for testing

--- include/debug_tools.h
+++ include/debug_tools.h
@@ -6,7 +6,7 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 18:34:37 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/06/28 15:05:12 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/07/22 17:14:43 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "libft.h"
 
 # ifndef DEBUG
-#  define DEBUG 0
+#  define DEBUG 1
 # endif
 # define UNREACHABLE "Unreachable."
 
--- src/repl.c
+++ src/repl.c
@@ -6,19 +6,21 @@
 /*   By: chuhlig <chuhlig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 16:07:04 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/07/15 19:53:52 by chuhlig          ###   ########.fr       */
+/*   Updated: 2024/07/22 17:13:51 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include "libft.h"
 #include "token.h"
 
 void	repl(const char *prompt)
 {
 	char	*input;
 	t_token	*token_list;
-	t_token *current;
-	t_token *next;
+	/* t_token *current; */
+	/* t_token *next; */
+	t_list *seq;
 
 	while (1)
 	{
@@ -28,13 +30,16 @@ void	repl(const char *prompt)
 		add_history(input);
 		token_list = NULL;
 		tokenizer(input, &token_list);
-		current = token_list;
-		while (current != NULL)
-		{
-			next = current->next;
-			free_token(current);
-			current = next;
-		}
-		free(input);
+		seq = parse(token_list);
+		if (seq != NULL)
+			print_ast(seq->content);
+		/* current = token_list; */
+		/* while (current != NULL) */
+		/* { */
+		/* 	next = current->next; */
+		/* 	free_token(current); */
+		/* 	current = next; */
+		/* } */
+		/* free(input); */
 	}
 }
