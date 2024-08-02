8ff71fbe1923a6a498173213a61a48e770b69ef0
Dominik Kaiser <dkaiser@2-B-2.42heilbronn.de>
2024-08-02 14:37:49 +0200

    Make collect_assigns.c norme conform

--- src/collect_assigns.c
+++ src/collect_assigns.c
@@ -6,13 +6,16 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 13:54:36 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/08/02 13:55:13 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/08/02 14:37:41 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "token.h"
 
 static t_assign	*to_assign(char *str);
+static int		count_tokens(t_token *tokens);
+static int		is_quote(char c);
 
 t_assign	**collect_assigns(t_token **tokens)
 {
@@ -20,38 +23,23 @@ t_assign	**collect_assigns(t_token **tokens)
 	t_assign	**result;
 	int			i;
 
+	result = malloc(sizeof(t_assign *) * (count_tokens(*tokens) + 1));
+	if (result == NULL)
+		return (free_tokens(*tokens), NULL);
 	cur = *tokens;
 	i = 0;
 	while (cur != NULL && cur->type == STRING_TOKEN
-		&& cur->content.string[0] != '"' && cur->content.string[0] != '\''
-		&& ft_strchr(cur->content.string, '=') != NULL)
+		&& !is_quote(cur->content.string[0]) && ft_strchr(cur->content.string,
+			'=') != NULL)
 	{
-		i++;
-		cur = cur->next;
-	}
-	result = malloc(sizeof(t_assign *) * (i + 1));
-	if (!result)
-	{
-		// free all tokens
-		return (NULL);
-	}
-	cur = *tokens;
-	i = 0;
-	while (cur != NULL && cur->type == STRING_TOKEN
-		&& cur->content.string[0] != '"' && cur->content.string[0] != '\''
-		&& ft_strchr(cur->content.string, '=') != NULL)
-	{
-		result[i] = to_assign(cur->content.string);
-		i++;
+		result[i++] = to_assign(cur->content.string);
 		if (cur->next != NULL)
 		{
 			cur = cur->next;
 			free_token(cur->previous);
 		}
 		else
-		{
 			free_token(cur);
-		}
 	}
 	*tokens = cur;
 	result[i] = NULL;
@@ -73,4 +61,24 @@ static t_assign	*to_assign(char *str)
 	result->var = str;
 	result->value = split_pos + 1;
 	return (result);
+}
+
+static int	count_tokens(t_token *tokens)
+{
+	int	len;
+
+	len = 0;
+	while (tokens != NULL && tokens->type == STRING_TOKEN
+		&& !is_quote(tokens->content.string[0])
+		&& ft_strchr(tokens->content.string, '=') != NULL)
+	{
+		len++;
+		tokens = tokens->next;
+	}
+	return (len);
+}
+
+static int	is_quote(char c)
+{
+	return (c == '"' || c == '\'');
 }
