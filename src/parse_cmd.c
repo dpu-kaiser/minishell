6779de5e02023366b37351f6590bed75e125c85b
Dominik Kaiser <dkaiser@2-B-2.42heilbronn.de>
2024-08-02 14:22:46 +0200

    Make parse_cmd.c norme conform

--- src/parse_cmd.c
+++ src/parse_cmd.c
@@ -6,12 +6,11 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:06:25 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/08/02 14:10:31 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/08/02 14:22:32 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "token.h"
 
 static char	**collect_args(t_token **tokens);
 
@@ -41,11 +40,8 @@ static char	**collect_args(t_token **tokens)
 		cur = cur->next;
 	}
 	result = malloc(sizeof(char *) * (i + 1));
-	if (!result)
-	{
-		free_tokens(*tokens);
-		return (NULL);
-	}
+	if (result == NULL)
+		return (free_tokens(*tokens), NULL);
 	cur = *tokens;
 	i = 0;
 	while (cur != NULL && cur->type == STRING_TOKEN)
