f0c751a453feb4d4652cd98f5bf485aeed7ce639
Dominik Kaiser <dkaiser@2-B-2.42heilbronn.de>
2024-08-02 15:00:04 +0200

    Some refactoring of collect_redirs

--- src/collect_redirs.c
+++ src/collect_redirs.c
@@ -6,39 +6,32 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 13:49:31 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/08/02 13:52:48 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/08/02 14:56:23 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static void		set_redir(t_redirection *redir, int type, char *specifier);
+static int		is_output_redir(int i);
 
 t_redirection	*collect_redirs(t_token **tokens)
 {
 	t_redirection	*result;
 	t_token			*cur;
-	int				idx;
 
 	cur = *tokens;
 	result = malloc(sizeof(t_redirection) * 2);
 	if (result == NULL)
-	{
-		// free all tokens
-		return (NULL);
-	}
+		return (free_tokens(*tokens), NULL);
 	set_redir(&result[0], 0, NULL);
 	set_redir(&result[1], 0, NULL);
 	while (cur != NULL && cur->next != NULL)
 	{
 		if (cur->type == REDIR_TOKEN && cur->next->type == STRING_TOKEN)
 		{
-			if (cur->content.redir_type & (INPUT_FILE | INPUT_LIMITER))
-				idx = 0;
-			else if (cur->content.redir_type & (OUTPUT_APPEND | OUTPUT_OVERRIDE))
-				idx = 1;
-			set_redir(&result[idx], cur->content.redir_type,
-				cur->next->content.string);
+			set_redir(&result[is_output_redir(cur->content.redir_type)],
+				cur->content.redir_type, cur->next->content.string);
 			cur = cur->next;
 			free_token_and_connect(cur->previous);
 			if (cur->next != NULL)
@@ -49,20 +42,15 @@ t_redirection	*collect_redirs(t_token **tokens)
 				free_token_and_connect(cur->previous);
 			}
 			else
-			{
 				free_token(cur);
-			}
 		}
 		else if (cur->type == REDIR_TOKEN)
 		{
-			// err Parsing error
-			free(result);
-			return (NULL);
+			dbg("TODO: Add parser error message");
+			return (free(result), NULL);
 		}
 		else
-		{
 			cur = cur->next;
-		}
 	}
 	return (result);
 }
@@ -71,4 +59,17 @@ static void	set_redir(t_redirection *redir, int type, char *specifier)
 {
 	redir->type = type;
 	redir->specifier = specifier;
+}
+
+static int	is_output_redir(int i)
+{
+	if (i & (INPUT_FILE | INPUT_LIMITER))
+		return (0);
+	else if (i & (OUTPUT_APPEND | OUTPUT_OVERRIDE))
+		return (1);
+	else
+	{
+		panic(UNREACHABLE);
+		return (-1);
+	}
 }
