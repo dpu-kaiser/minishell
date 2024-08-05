d3474100c72be3384d8fe8ca9e6a24260d0c1b52
Christopher Uhlig <chuhlig@2-D-4.42heilbronn.de>
2024-08-05 13:21:45 +0200

    updated the date in header

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: chuhlig <chuhlig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 17:14:03 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/06/25 13:58:24 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/07/18 16:44:14 by chuhlig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../include/minishell.h"
 
 int	main(void)
 {
