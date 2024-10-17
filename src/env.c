a0016dffa6c5412d7c53b3d9040b837299c50159
Christopher Uhlig <chuhlig@1-C-5.42heilbronn.de>
2024-10-17 14:32:28 +0200

    added env linked lis c file

---
+++ src/env.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuhlig <chuhlig@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 14:31:07 by chuhlig           #+#    #+#             */
+/*   Updated: 2024/10/17 14:31:50 by chuhlig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+
+void	getenvlst(t_env **env, char **en)// seperated name and value
+{
+	char	*tmp;
+	int		i;
+	t_env 	*current;
+
+	i = 0;
+	while (en[i] != NULL)
+	{
+		tmp = ft_strchr(en[i], '=');
+		tmp = '\0';
+		current = *env;
+        current = malloc(sizeof(t_env));
+        current->name = ft_strdup(en[i]);
+        current->value = ft_strdup(tmp + 1);
+        current->next = *env;
+        *env = current;
+		i++;
+	}
+	return (0);
+}
+
+
+void	free_envlst(t_env **env)
+{
+	t_env	*cur;
+	t_env 	*new;
+
+	cur = *env;
+	while (cur)
+	{
+		new = cur->next;
+		free(cur->name);
+		free(cur->value);
+		free(cur);
+		cur = new;
+	}
+	
+}
