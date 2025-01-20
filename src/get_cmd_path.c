af9d1a9b39daaf1b86cf94ee629e06503d8ab6d4
Dominik Kaiser <dkaiser@3-H-1.42heilbronn.de>
2025-01-20 13:07:11 +0100

    Fix some bugs

--- src/get_cmd_path.c
+++ src/get_cmd_path.c
@@ -6,7 +6,7 @@
 /*   By: chuhlig <chuhlig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 19:19:59 by chuhlig           #+#    #+#             */
-/*   Updated: 2025/01/15 16:38:39 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/01/20 12:52:00 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,8 +85,9 @@ static char	*find_in_path(char *cmd, t_env *env, int *return_code)
 		path++;
 	}
 	*return_code = 127;
-	printf("%s:", cmd);
+	ft_printf("%s:", cmd);
 	ft_putstr_fd(" command not found", 2);
+	ft_printf("\n");
 	return (NULL);
 }
 
