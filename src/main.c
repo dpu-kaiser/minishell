0bfb0d10ee31f15823e6e999818c9ec95d8f8f4b
Dominik Kaiser <dkaiser@3-H-7.42heilbronn.de>
2025-01-25 17:57:44 +0100

    Remove promptflag

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: chuhlig <chuhlig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 17:14:03 by dkaiser           #+#    #+#             */
-/*   Updated: 2025/01/25 15:41:04 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/01/25 17:57:17 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_env		*env;
-	static int	promptflag;
 
 	env = NULL;
-	promptflag = 0;
 	if (!argc && !argv)
 		return (1);
 	if (init())
